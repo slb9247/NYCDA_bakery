2afff91d60f7fed718bf5661d0d723469c430460
Samantha Bolotine <slb1992@yahoo.com>
2016-05-31 20:05:23 -0400

    jquery fixes

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).on('ready', function() {
 
 //scrollTo
-  $('nav a').on('click', function(event) {
+  $('nav a, .content-bakery a').on('click', function(event) {
     console.log(event);
     event.preventDefault();
 
@@ -16,74 +16,62 @@ $(document).on('ready', function() {
   });
 
 
-//image hovers
-  $('#img-cannoli').on('mouseenter', function() {
-    $('#hover-cannoli').fadeTo(500, 1.0);
-  });
-  $('#img-cannoli').on('mouseleave', function() {
-    $('#hover-cannoli').hide();
-  });
+// show and hide text on hovered and non-hovered images
+  // $('#img-cannoli').on('mouseenter', function() {
+  //   $('#hover-cannoli').fadeTo(500, 1.0);
+  // });
+  // $('#img-cannoli').on('mouseleave', function() {
+  //   $('#hover-cannoli').hide();
+  // });
 
-  $('#img-puff').on('mouseenter', function() {
-    $('#hover-puff').fadeTo(500, 1.0);
+  $('.image').on('mouseenter', function() {
+    $(this).find('.hover-text').fadeTo(500, 1.0);
   });
-  $('#img-puff').on('mouseleave', function() {
-    $('#hover-puff').hide();
-  });
-
-  $('#img-eclair').on('mouseenter', function() {
-    $('#hover-eclair').fadeTo(500, 1.0);
-  });
-  $('#img-eclair').on('mouseleave', function() {
-    $('#hover-eclair').hide();
-  });
-
-  $('#img-whoopie').on('mouseenter', function() {
-    $('#hover-whoopie').fadeTo(500, 1.0);
-  });
-  $('#img-whoopie').on('mouseleave', function() {
-    $('#hover-whoopie').hide();
-  });
-
-  $('#img-cake').on('mouseenter', function() {
-    $('#hover-cake').fadeTo(500, 1.0);
-  });
-  $('#img-cake').on('mouseleave', function() {
-    $('#hover-cake').hide();
-  });
-
-  $('#img-pie').on('mouseenter', function() {
-    $('#hover-pie').fadeTo(500, 1.0);
-  });
-  $('#img-pie').on('mouseleave', function() {
-    $('#hover-pie').hide();
+  $('.image').on('mouseleave', function() {
+    $(this).find('.hover-text').hide();
   });
 
 
+
+
+// images click events
   $('#img-cannoli').on('click', function() {
-    $('#img-puff').hide();
-    $('#img-eclair').hide();
-    $('#img-whoopie').hide();
+    $('#img-puff, #img-eclair, #img-whoopie').hide();
+  //  $('#show').css('display', 'inline-block');
+    $('#flavors-cannoli').fadeIn(2000);
+    $('#button-pastries').show();
   });
 
   $('#img-puff').on('click', function() {
-    $('#img-cannoli').hide();
-    $('#img-eclair').hide();
-    $('#img-whoopie').hide();
+    $('#img-cannoli, #img-eclair, #img-whoopie').hide();
+    $('#flavors-puff').fadeIn(2000);
+    $('#button-pastries').show();
   });
 
   $('#img-eclair').on('click', function() {
-    $('#img-cannoli').hide();
-    $('#img-puff').hide();
-    $('#img-whoopie').hide();
+    $('#img-cannoli, #img-puff, #img-whoopie').hide();
+    $('#flavors-eclair').fadeIn(2000);
+    $('#button-pastries').show();
   });
 
   $('#img-whoopie').on('click', function() {
-    $('#img-cannoli').hide();
-    $('#img-puff').hide();
-    $('#img-eclair').hide();
+    $('#img-cannoli, #img-puff, #img-eclair').hide();
+    $('#flavors-whoopie').fadeIn(2000);
+    $('#button-pastries').show();
   });
 
+
+  $('#img-cake').on('click', function() {
+    $('#img-pie').hide();
+    //fade in flavors
+    $('#button-cakesPies').show();
+  });
+
+  $('#img-pie').on('click', function() {
+    $('#img-cake').hide()
+    //fade in flavors
+    $('#button-cakesPies').show();
+  });
 
 
 }); // ends doc.ready
