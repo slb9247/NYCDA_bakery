4afb829a63e7dd8072ca23799684435a243e0e0d
Samantha Bolotine <slb1992@yahoo.com>
2016-06-01 23:30:34 -0400

    bug fixes and contact page

--- js/main.js
+++ js/main.js
@@ -1,14 +1,22 @@
 $(document).on('ready', function() {
 
+  // // run test on initial page load
+  // checkSize();
+  //
+  // // run test on window resize
+  // $(window).resize(checkSize);
+
+
+
 //scrollTo
   $('nav a, .content-bakery a').on('click', function(event) {
     console.log(event);
     event.preventDefault();
 
     // scroll to the page associated with THIS link
-    //console.log(this);
+    // console.log(this);
     var pageToScrollTo = $(this).attr('href');
-    //console.log(pageToScrollTo)
+    // console.log(pageToScrollTo)
 
     // use pageToScrollTo in a .scrollTo function
     $(window).scrollTo(pageToScrollTo, 800, {offset: -100});
@@ -28,61 +36,53 @@ $(document).on('ready', function() {
 
 
 // images click events
-  $('#img-cannoli').on('click', function() {
-    $('#img-puff, #img-eclair, #img-whoopie').hide();
-  //  $('#show').css('display', 'inline-block');
-    $('#flavors-cannoli').fadeIn(2000);
-    $('#sub-pastries').css('height', '750px');
+  $('.image-pastry').on('click', function() {
+    // hide all images
+    $('.image-pastry').addClass('hide');
+    // show image that was clicked
+    $(this).removeClass('hide');
+    // place display: inline-block on the list
+    $(this).next('.list-pastries').css('display', 'inline-block');
+    // fade in the flavors
+    $(this).next('.list-pastries').find('.flavors').fadeIn(2000);
+    // show pastries back button
     $('#button-pastries').show();
   });
 
-  $('#img-puff').on('click', function() {
-    $('#img-cannoli, #img-eclair, #img-whoopie').hide();
-    $('#flavors-puff').fadeIn(2000);
-    $('#sub-pastries').css('height', '750px');
-    $('#button-pastries').show();
-  });
-
-  $('#img-eclair').on('click', function() {
-    $('#img-cannoli, #img-puff, #img-whoopie').hide();
-    $('#flavors-eclair').fadeIn(2000);
-    $('#sub-pastries').css('height', '750px');
-    $('#button-pastries').show();
-  });
-
-  $('#img-whoopie').on('click', function() {
-    $('#img-cannoli, #img-puff, #img-eclair').hide();
-    $('#flavors-whoopie').fadeIn(2000);
-    $('#sub-pastries').css('height', '750px');
-    $('#button-pastries').show();
-  });
-
-  $('#img-cake').on('click', function() {
-    $('#img-pie').hide();
-    $('#flavors-cakes').fadeIn(2000);
-    $('#sub-cakesPies').css('height', '700px');
+  $('.image-cakePie').on('click', function() {
+    // hide all images
+    $('.image-cakePie').addClass('hide');
+    // show image that was clicked
+    $(this).removeClass('hide');
+    // place display: inline-block on the list
+    $(this).next('.list-cakesPies').css('display', 'inline-block');
+    // fade in the flavors
+    $(this).next('.list-cakesPies').find('.flavors').fadeIn(2000);
+    // show pastries back button
     $('#button-cakesPies').show();
   });
 
-  $('#img-pie').on('click', function() {
-    $('#img-cake').hide()
-    $('#flavors-pies').fadeIn(2000);
-    $('#sub-cakesPies').css('height', '700px');
-    $('#button-cakesPies').show();
-  });
+
 
 // back button click events
-  // $('#button-pastries').on('click', function() {
-  //   $('.flavors').hide();
-  //   $('#img-cannoli, #img-puff, #img-eclair, #img-whoopie').show();
-  //   $('#sub-pastries').css('height', '1100px');
-  //   $('#button-pastries').hide();
-  // });
+  $('#button-pastries').on('click', function() {
+    // hide button
+    $(this).hide();
+    // hide all lists
+    $('.list-pastries').hide();
+    // show all images
+    $('.image-pastry').removeClass('hide');
+  });
 
-  // $('#button-cakesPies').on('click', function() {
-  //   $('#flavors-cakes, #flavors-pies, #button-cakesPies').hide();
-  //   $('#img-cake, #img-pie').show();
-  // });
+  $('#button-cakesPies').on('click', function() {
+    // hide button
+    $(this).hide();
+    // hide all lists
+    $('.list-cakesPies').hide();
+    // show all images
+    $('.image-cakePie').removeClass('hide');
+  });
+
 
 
 }); // ends doc.ready
